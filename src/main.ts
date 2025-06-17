8eb6d9870b8fba510ada9c59f88ced1b57b3f04f
CDomins <christiandomins07@gmail.com>
2025-06-17 09:01:41 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+import { UpgradeModule } from '@angular/upgrade/static';
+import './assets/legacy/js/app.js';
+import './assets/legacy/js/controllers/taskController.js';
+import './assets/legacy/js/services/taskService.js';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((platformRef) => {
+    const upgrade = platformRef.injector.get(UpgradeModule);
+    upgrade.bootstrap(document.body, ['taskApp']);
+  })
+  .catch((err) => console.error(err));
